012e77a6b4bd61055202caf22abfc444ca3f48bd
berrhandz <sberridge7@gmail.com>
2025-05-28 21:57:41 -0500

    initial commit

---
+++ src/select1.tsx
@@ -0,0 +1,19 @@
+
+import { Select } from "@mantine/core";
+
+function select1() {
+    //do stuff
+    //const [state, otherState] = useState('h');
+    //return component
+    return (
+        <>
+            <div>
+                <Select id="sam">
+
+                </Select>
+            </div>
+        </>
+    );
+}
+
+export default select1
