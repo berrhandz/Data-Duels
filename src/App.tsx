3c7b8c0ebbf1e5cfe4c9e121583949b085cdeeba
berrhandz <sberridge7@gmail.com>
2025-05-31 01:57:18 -0500

    added select for teams... should make component out of this guy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react'
+import { Select, MantineProvider } from '@mantine/core'
+import '@mantine/core/styles.css';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -9,25 +11,14 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <MantineProvider>
+          <Select className='select'
+            label="Pick A Team"
+            placeholder='Pick Value'
+            data={['react', 'angular', 'vue', 'svelte']}
+          />
+        </MantineProvider>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
