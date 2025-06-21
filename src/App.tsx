343d523b890405832bc91dccdbf4258550270b9a
berrhandz <sberridge7@gmail.com>
2025-06-20 20:18:45 -0500

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react'
-import { Select, MantineProvider } from '@mantine/core'
+import { Select, MantineProvider, Table } from '@mantine/core'
 import team from '../team.json'
 import '@mantine/core/styles.css';
 import './App.css'
@@ -11,15 +11,50 @@ const teams = team.map((x) => {
 })
 console.log(teams)
 function App() {
-  const [count, setCount] = useState(0)
+  const [team1, setTeam1] = useState('Team 1')
 
+  function teamSelect() {
+    const [team1, setTeam1] = useState('Team 1')
+    return <Select className='select'
+      label="Pick A Team"
+      placeholder={team1}
+      data={teams}
+      value={team1 ? team1 : null}
+      onChange={(_value) => setTeam1(String(_value))}
+      allowDeselect
+    />
+
+  }
   return (
     <>
       <div>
         <MantineProvider>
+          <Table>
+            <Table.Thead>
+              <Table.Tr>
+                <Table.Th colSpan={5}>Element position</Table.Th>
+                <Table.Th colSpan={5}>Element name</Table.Th>
+              </Table.Tr>
+            </Table.Thead>
+            <Table.Tbody>{ }</Table.Tbody>
+          </Table>
+        </MantineProvider>
+      </div>
+      <div>
+        <MantineProvider>
           <Select className='select'
             label="Pick A Team"
-            placeholder='Pick Value'
+            placeholder={team1}
+            data={teams}
+            defaultValue={'Pick a Team'}
+          />
+        </MantineProvider>
+      </div>
+      <div>
+        <MantineProvider>
+          <Select className='select'
+            label="Pick A Team"
+            placeholder={team1}
             data={teams}
           />
         </MantineProvider>
